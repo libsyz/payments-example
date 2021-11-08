33adcdfacb6098d7b4c277334ef60e4142f119bb
Miguel Jimenez <denkatalabeno@gmail.com>
2021-11-08 14:29:48 +0800

    added price with money gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,19 @@ geek = Category.create!(name: 'geek')
 kids = Category.create!(name: 'kids')
 
 puts 'Creating teddies...'
-Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
-
-Teddy.create!(sku: 'prima-sku', name: 'Prima Aulia - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Teddy.create!(sku: 'miguel-sku', name: 'miguel-octo -  GitHub', category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+Teddy.create!(sku: 'original-teddy-bear',
+              name: 'Teddy bear',
+              category: kids,
+              photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg',
+              price: 6_000)
+Teddy.create!(sku: 'prima-sku',
+              name: 'Prima Aulia - Le Wagon',
+              category: geek,
+              photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large',
+              price: 7_000)
+Teddy.create!(sku: 'miguel-sku',
+              name: 'miguel-octo -  GitHub',
+              category: geek,
+              photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg',
+              price: 8_000)
 puts 'Finished!'
