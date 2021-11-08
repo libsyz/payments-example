b3f299ef3eb5c0c76cd3d0b99e95825c8dbaf6a3
Miguel Jimenez <denkatalabeno@gmail.com>
2021-11-08 14:38:00 +0800

    added order controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
